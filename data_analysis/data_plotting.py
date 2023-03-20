Fixed data computation, refactored, misc.

data_analysis/data_plotting.py
@@ -11,31 +11,40 @@ def plot_efficiency_vs_mirror_angle(data, figure_number:int, **kwargs):
     Valid kwargs:
     point_shape: pyplot data point shape.
     label_prefix: what to prefix grating angles for this set of data"""
-
-    # Grab valid kwargs
-    point_shape = kwargs.pop("point_shape", None)
+    # Get valid kwargs
     label_prefix = kwargs.pop("label_prefix", None)
-
-    #Tells the plotter to do operations on the given figure, referenced via the figure number
+    point_shape = kwargs.pop("point_shape", None)
+    # Move plotter to the provided figure number
     plt.figure(figure_number)
-
-    # Loop over the grating angles and their associated efficiency vs. mirror angle data.
-    for grating_angle, data in data.items():
-        # Set the label. Prefix the series plot label with label_prefix and append the grating angle, else simply the grating angle
-        label = f"{label_prefix}: {grating_angle}" if label_prefix is not None else grating_angle
-        # Plot the data series. This assumes that the 1st and 2nd columns in the data array correspond to mirror angle and efficiency, respectively. In addition, set the label as well as the point shape (the latter if provided. Else simply a dot.)
-        plt.scatter(data[:, 0], data[:, 1], s=8.0, label=label, marker=(point_shape if point_shape is not None else ".")) # Plot efficiency vs mirror angle and label the series as "angle" where angle is the numerical angle (adjusted to normal incidence)
-    
-    # Put some cosmetic things to make the plot understandable.
-    plt.legend(loc="upper left")
-    plt.legend(loc="best")
-#    plt.title("log of Efficiency vs. Mirror angle, with efficiency < 0.01 removed")
-    plt.title("log of Efficiency vs. Mirror angle")
-    # plt.margins(y=10.0)
-    plt.grid(which="both", axis="both")
-    plt.yscale("log")
+    # Loop over grating angles and their respective data
+    for grating_angle, efficiencies_vs_mirror_angle in data.items():
+        # Set label_prefix if provided, otherwise make it an empty string
+        label_prefix = label_prefix if label_prefix is not None else ""
+        # Plot the unmasked data first
+        plt.scatter(
+            efficiencies_vs_mirror_angle[:, 0],
+            efficiencies_vs_mirror_angle[:, 1],
+            label=f"{label_prefix}{grating_angle}", 
+            marker=(point_shape if point_shape is not None else "."),
+            s=50
+            )
+        # Now plot the masked data as a separate series
+        plt.scatter(
+            np.asarray(efficiencies_vs_mirror_angle[:, 0][efficiencies_vs_mirror_angle[:, 1].mask]),
+            np.asarray(efficiencies_vs_mirror_angle[:, 1][efficiencies_vs_mirror_angle[:, 1].mask]),
+            label=f"{label_prefix}Approximated A at {grating_angle}",
+            marker=(point_shape if point_shape is not None else "."),
+            s=50
+            )
+    # Set cosmetic figure things to make it human readable, or whatever. Most importantly, put the legend away from the plotted region.
+    plt.legend(bbox_to_anchor=(1.10, 1), loc='upper right', borderaxespad=0)
+    plt.ticklabel_format(axis="y") #, style="sci", scilimits=(0,0)
+    plt.grid(which="both", axis='both')
     plt.xlabel('mirror angle ($^\circ$)')
+    plt.yscale("log")
     plt.ylabel('$log_{10}$(efficiency)')
+    plt.suptitle("Efficiency vs. mirror angle")
+    return
 
 def plot_efficiency_vs_grating_angle(data:np.array, figure_number:int, **kwargs):
     """Plots efficiency vs grating angle for the provided data. Requires the figure number to plot on.
@@ -45,9 +54,9 @@ def plot_efficiency_vs_grating_angle(data:np.array, figure_number:int, **kwargs)
     label = kwargs.pop("label", None)
     plt.figure(figure_number)
     if label is not None:
-        plt.scatter(data[:, 0], data[:, 1], s=10.0, label=label) #plot data
+        plt.plot(data[:, 0], data[:, 1], label=label) #plot data
     else:
-        plt.scatter(data[:, 0], data[:, 1], s=10.0) #plot data
+        plt.plot(data[:, 0], data[:, 1]) #plot data
     plt.title("Efficiency vs. grating angle")
     # plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
     plt.xlabel('grating angle ($^\circ$)')
@@ -89,7 +98,6 @@ def plot_powers_vs_mirror_angle(data:dict, figure_number:int, **kwargs):
     
     Optional arguments:
     label_prefix: A string to prefix prior to the series in the legend."""
-
     # Get valid kwargs
     label_prefix = kwargs.pop("label_prefix", None)
     # Move plotter to the provided figure number
@@ -104,9 +112,12 @@ def plot_powers_vs_mirror_angle(data:dict, figure_number:int, **kwargs):
         # Chose v for power B's marker just because it's lower in the experiment.
         # Chose x for incident power because it's like a vector going into the experiment.
         # I know. Crappy, right?
-        plt.scatter(powers_vs_mirror_angle[:, 0], powers_vs_mirror_angle[:, 1], s=5.0, label=f"{label_prefix}A at {grating_angle}")
+        plt.scatter(powers_vs_mirror_angle[:, 0], powers_vs_mirror_angle[:, 1], s=5.0, label=f"{label_prefix}A at {grating_angle}", marker="v")
+        plt.scatter(np.asarray(powers_vs_mirror_angle[:, 0][powers_vs_mirror_angle[:, 1].mask]), np.asarray(powers_vs_mirror_angle[:, 1][powers_vs_mirror_angle[:, 1].mask]), s=5.0, label=f"{label_prefix}Approximated A at {grating_angle}")
         plt.scatter(powers_vs_mirror_angle[:, 0], powers_vs_mirror_angle[:, 2], s=5.0, label=f"{label_prefix}B at {grating_angle}", marker="v")
+        plt.scatter(np.asarray(powers_vs_mirror_angle[:, 0][powers_vs_mirror_angle[:, 2].mask]), np.asarray(powers_vs_mirror_angle[:, 2][powers_vs_mirror_angle[:, 2].mask]), s=5.0, label=f"{label_prefix}Approximated B at {grating_angle}")
         plt.scatter(powers_vs_mirror_angle[:, 0], powers_vs_mirror_angle[:, 3], s=5.0, label=f"{label_prefix}Incident at {grating_angle}", marker="x")
+        plt.scatter(np.asarray(powers_vs_mirror_angle[:, 0][powers_vs_mirror_angle[:, 3].mask]), np.asarray(powers_vs_mirror_angle[:, 3][powers_vs_mirror_angle[:, 3].mask]), s=5.0, label=f"{label_prefix}Approximated Incident at {grating_angle}")
     # Set cosmetic figure things to make it human readable, or whatever. Most importantly, put the legend away from the plotted region.
     plt.legend(bbox_to_anchor=(1.10, 1), loc='upper right', borderaxespad=0)
     plt.ticklabel_format(axis="y") #, style="sci", scilimits=(0,0)