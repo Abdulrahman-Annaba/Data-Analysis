e2a49f6329edae5333f99f874064ee9061d2a846
Abdulrahman Annaba <abdulrahman.annaba@gmail.com>
2023-04-12 09:10:37 -0500

    Added wood's anomaly predictions Fixed bug in spr angle predictions to be symmetric across zero incidence

--- data_analysis/theory.py
+++ data_analysis/theory.py
@@ -2,19 +2,21 @@ import numpy as np
 
 class Grating:
 
-    def __init__(self, groove_spacing:int=1200, e_m:float=-46.6, wavelength: float=640, e_d:float=1):
+    def __init__(self, groove_spacing:int=1200, e_m:float=-46.6, wavelength: float=640, e_d:float=1, epsilon:float=1):
         """Creates a grating object.
         
         :param groove_spacing: the number of grooves per millimeter in the grating.
         :param e_m: the dielectric constant of the grating material.
         :param wavelength: the wavelength of the incoming light, in nanometers.
         :param e_d: the dielectric constant of the other medium at the grating interface.
+        :param epsilon: the angle that the plane of incidence makes with the grating normal, in degrees. Positive values indicate a decline.
         """
         self.groove_spacing = groove_spacing
         self.e_m = e_m
         self.wavelength = wavelength
         self.e_d = e_d
-    
+        self.epsilon = epsilon
+
     def find_diffraction_orders(
         self,
         wavelength:float,
@@ -68,6 +70,27 @@ class Grating:
             incident_angle = np.arcsin(constraint - m*wavelength*groove_spacing)*180/np.pi
             if incident_angle <= 90 and incident_angle >= -90:
                 result.update({str(m): incident_angle})
+        flipped_results = dict()
+        for order, angle in result.items():
+            flipped_results.update({str(-1*int(order)) : -1*angle})
+        result.update(flipped_results)
+        return result
+    
+    def woods_anomaly_angles(self, n_orders: int):
+        """Computes the incident angles at which wood's anomaly occurs, according to the equation sin(incident_angle) = m*wavelength*groove_spacing/cos(epsilon) +/- 1, where epsilon is the angle between the grating normal and the plane of incidence in degrees.
+        
+        :param n_orders: the total number of orders to consider. This is halved to account for the negative orders.
+        :return: a dictionary of order numbers and incident angles representing the locations of wood's anomalies."""
+        orders = divmod(n_orders, 2)[0]
+        wavelength = self.wavelength*10**-9
+        groove_spacing = self.groove_spacing*10**3
+        epsilon = self.epsilon * np.pi/180
+
+        result = dict()
+        for m in range(-orders, orders + 1, 1):
+            incident_angle = np.arcsin(m*wavelength*groove_spacing/np.cos(epsilon) + 1) * 180/np.pi
+            if incident_angle <= 90 and incident_angle >= -90:
+                result.update({str(m): incident_angle})
         return result
 
     @staticmethod
@@ -85,13 +108,14 @@ class Grating:
 
 if __name__ == "__main__":
     """Example usage"""
-    incident_angle = 90
-    wavelength = 640
-    epsilon = (17.80*10**-3)/(24.5*10**-2)*180/np.pi
+    wavelength = 637.3
+    epsilon = 3.8914
     groove_spacing = 1200
 
-    e_m = -46.6
-    e_d = 1^2
-    grating = Grating(groove_spacing, e_m, wavelength, e_d)
+    e_m = -56.96
+    e_d = 1.00**2
+    grating = Grating(groove_spacing, e_m, wavelength, e_d, epsilon)
 
     print(grating.spr_angles(10))
+
+    print(grating.woods_anomaly_angles(10))
