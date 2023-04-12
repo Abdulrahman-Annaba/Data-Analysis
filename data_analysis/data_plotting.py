f5b7b2556f7dad7544702ba607dd1ba87453175b
Abdulrahman Annaba <abdulrahman.annaba@gmail.com>
2023-04-12 09:35:15 -0500

    Added wood's anomaly prediction to plot function

--- data_analysis/data_plotting.py
+++ data_analysis/data_plotting.py
@@ -42,9 +42,11 @@ def plot_efficiency_vs_incident_angle(data:np.array, figure_number:int, **kwargs
     
     kwargs:
     label: label to add to figure
-    spr_angles: a dictionary of diffraction orders and incident angles at which SPR can occur. When provided, adds vertical lines at these incident angles demonstrating locations of theoretical predictions for SPR"""
+    spr_angles: a dictionary of diffraction orders and incident angles at which SPR can occur. When provided, adds vertical lines at these incident angles demonstrating locations of theoretical predictions for SPR
+    woods: a dictionary of diffraction orders and incident angles at which woods anomalies can occur. When provided, adds vertical lines at these incident angles demonstrating locations of theoretical predictions for woods anomalies"""
     label = kwargs.pop("label", None)
     spr_angles = kwargs.pop("spr_angles", None)
+    woods = kwargs.pop("woods", None)
     plt.figure(figure_number)
     if label is not None:
         plt.plot(data[:, 0], data[:, 1], label=label) #plot data
@@ -53,6 +55,9 @@ def plot_efficiency_vs_incident_angle(data:np.array, figure_number:int, **kwargs
     if spr_angles is not None:
         for order, spr_angle in spr_angles.items():
             plt.axvline(x=spr_angle, color='b', label=f"{label}: SPR order {order}")
+    if woods is not None:
+        for order, wood_angle in woods.items():
+            plt.axvline(x=wood_angle, color='r', label=f"{label}: Wood anomaly {order}")
     plt.title("Efficiency vs. incident angle")
     # plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
     plt.xlabel('incident angle ($^\circ$)')
